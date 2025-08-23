e5d66307169d8d8c5ad6275ea426466e5f49ce82
M Asaad Shaikh <Mohmmed Asaad Shaikh>
2025-08-23 16:40:27 +1200

    KAN-37 Utils function added + medicines added

--- controllers/utils/functions.js
+++ controllers/utils/functions.js
@@ -1,4 +1,6 @@
 const bcrypt = require('bcryptjs');
+const { UpdateItemCommand, DynamoDBClient } = require('@aws-sdk/client-dynamodb');
+const client = new DynamoDBClient({ region: process.env.AWS_REGION });
 
 const generateHashedPassword = async (password) => {
     const saltRounds = 10;
@@ -12,7 +14,33 @@ const verifyPassword = async (password, hashedPassword) => {
     return result;
 };
 
+/**
+ * Fetches and atomically increments a counter.
+ * @param {string} id_name - Table name (e.g., 'Orders')
+ * @returns {Promise<number>} - Next numeric value of the counter
+ */
+const getNextId = async (id_name) => {
+
+    const command = new UpdateItemCommand({
+        TableName: 'OrderCounter',
+        Key: {
+            id: { S: id_name },
+        },
+        UpdateExpression: 'SET lastValue = if_not_exists(lastValue, :start) + :inc',
+        ExpressionAttributeValues: {
+            ':start': { N: '0' },
+            ':inc': { N: '1' },
+        },
+        ReturnValues: 'UPDATED_NEW',
+    });
+
+    const result = await client.send(command);
+
+    return result.Attributes.lastValue.N;
+};
+
 module.exports = {
     generateHashedPassword,
     verifyPassword,
+    getNextId,
 };
