2ac314d51e8f5c49a045a3137e279d36416c730e
M Asaad Shaikh <Mohmmed Asaad Shaikh>
2025-08-23 16:39:56 +1200

    KAN-37 Pharma login updated

--- controllers/pharmacy.js
+++ controllers/pharmacy.js
@@ -1,37 +1,58 @@
+const { QueryCommand, DynamoDBClient } = require('@aws-sdk/client-dynamodb');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+require('dotenv').config({ quiet: true });
+
+const client = new DynamoDBClient({ region: process.env.AWS_REGION });
 
 const loginPharma = async (req, res) => {
     const { email, password } = req.body;
-    // Check if
-    // 1. User exists in pharma DB
-    // 2. Password is correct
 
-    // TEST
-    const user = {
-        name: 'John Doe',
-        email: 'johndoe@gmail.com',
-        age: 43,
-        type: 'pharma',
-        _id: 1,
-    };
+    try {
+        const command = new QueryCommand({
+            TableName: 'Pharmacy',
+            IndexName: 'email-index',
+            KeyConditionExpression: 'email = :email',
+            ExpressionAttributeValues: {
+                ':email': { S: email },
+            },
+        });
 
-    const token = jwt.sign({ _id: user?._id }, process.env.JWT_SECRET, { expiresIn: '30m' });
-    res.cookie('jwt_pharma', token, {
-        httpOnly: true,
-        maxAge: 1800000,
-        secure: process.env.ENVIRONMENT === 'prod',
-        sameSite: 'Lax',
-    });
+        const response = await client.send(command);
+        const pharma = response.Items?.[0];
 
-    const userData = {
-        name: user?.name,
-        email: user?.email,
-        age: user?.age,
-        type: user?.type,
-        token,
-    };
+        if (!pharma) return res.status(404).json({ success: false, message: 'Pharmacy not found' });
 
-    res.status(200).json({ success: true, message: 'Login successful', data: userData });
+        const hashedPassword = pharma.password.S;
+        const isMatch = await bcrypt.compare(password, hashedPassword);
+        if (!isMatch) return res.status(401).json({ success: false, message: 'Invalid credentials' });
+
+        const token = jwt.sign({ _id: pharma.id.S }, process.env.JWT_SECRET, { expiresIn: '30m' });
+        res.cookie('jwt_pharma', token, {
+            httpOnly: true,
+            maxAge: 1800000,
+            secure: process.env.ENVIRONMENT === 'prod',
+            sameSite: 'Lax',
+        });
+
+        const userData = {
+            id: pharma.id.S,
+            first_name: pharma.first_name?.S,
+            last_name: pharma.last_name?.S,
+            email: pharma.email.S,
+            type: 'Pharma',
+            token,
+        };
+
+        return res.status(200).json({
+            success: true,
+            message: 'Login successful',
+            data: userData,
+        });
+    } catch (err) {
+        console.error('Pharma login error:', err);
+        return res.status(500).json({ success: false, message: 'Internal server error' });
+    }
 };
 
 module.exports = {
