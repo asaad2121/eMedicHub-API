5b476ff8243a767dda754b774a8af88e0970ca73
asaad2121 <md.asaad.shaikh@gmail.com>
2025-09-15 14:48:44 +1200

    Signup page queries fixed

--- routes/patients.js
+++ routes/patients.js
@@ -9,7 +9,7 @@ const {
 } = require('../controllers/patients');
 const { check, validationResult } = require('express-validator');
 const { authenticateToken, authenticateRefreshToken } = require('../middleware/session-authentication-middleware');
-const { getAllDoctors, addNewPatientPost } = require('../controllers/doctors');
+const { getAllDoctors, addNewPatientPost, addNewPatientGet } = require('../controllers/doctors');
 const { getUserProfile, resetPassword } = require('../controllers/userInfo');
 
 router.get('/getUserProfile/:id', (req, res, next) => authenticateToken('patient', req, res, next), getUserProfile);
@@ -35,7 +35,7 @@ router.post(
 );
 
 router.get('/getDoctors', (req, res, next) => authenticateToken('patient', req, res, next), getAllDoctors);
-router.get('/viewAppointments', (req, res, next) => authenticateToken('patient', req, res, next), viewAppointments);
+router.get('/viewAppointments', (req, res, next) => authenticateToken(req.query.type, req, res, next), viewAppointments);
 router.get(
     '/viewAppointmentData',
     (req, res, next) => authenticateToken('patient', req, res, next),
@@ -105,7 +105,41 @@ router.post(
     loginPatients
 );
 
-router.get('/signup', (req, res, next) => authenticateToken('patient', req, res, next), addNewPatientPost);
+router.post(
+    '/signup',
+    [
+        check('id', 'Patient ID cannot be empty').notEmpty(),
+        check('id', 'Patient ID must contain 7 characters').isLength({ min: 7, max: 9 }),
+        check('email', 'Email cannot be empty').notEmpty(),
+        check('email', 'Email is not Valid')
+            .matches(/.+\@.+\..+/)
+            .isLength({ min: 4, max: 35 }),
+        check('first_name', 'First Name is required').notEmpty(),
+        check('first_name', 'First Name must contain 2 characters').isLength({ min: 2, max: 64 }),
+        check('last_name', 'Last Name is required').notEmpty(),
+        check('last_name', 'Last Name must contain 2 characters').isLength({ min: 2, max: 64 }),
+        check('password', 'Password is required').notEmpty(),
+        check('password', 'Password must contain 6 characters').isLength({ min: 6, max: 64 }),
+        check('password', 'Password must contain a digit').matches(/\d/),
+        check('blood_grp', 'Blood group is required').notEmpty(),
+        check('phone_no', 'Phone number is required').notEmpty(),
+        check('phone_no', 'Phone number must be exactly 10 digits eg 0221231234').matches(/^\d{10}$/),
+        check('gp_id', 'GP ID is required').notEmpty(),
+        check('id_type', 'Identification Type is required').notEmpty(),
+        check('id_number', 'Identification Number is required').notEmpty(),
+    ],
+    (req, res, next) => {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            res.status(400).json({ success: false, error: errors.array() });
+            return;
+        }
+        next();
+    },
+    addNewPatientPost
+);
+router.get('/signup', addNewPatientGet);
 
 router.get('/logout', (req, res) => {
     res.clearCookie('jwt_patient');
