7e6feae1f33141c9e4e417baffbf09ca19ef8319
asaad2121 <md.asaad.shaikh@gmail.com>
2025-09-23 19:25:20 +1200

    Increase rate limiter

--- controllers/utils/functions.js
+++ controllers/utils/functions.js
@@ -6,7 +6,7 @@ const rateLimit = require('express-rate-limit');
 
 const refreshLimiter = rateLimit({
     windowMs: 1 * 60 * 1000,
-    max: 5,
+    max: 15,
     message: { success: false, message: 'Too many refresh attempts, please try again later' },
     standardHeaders: true,
     legacyHeaders: false,
